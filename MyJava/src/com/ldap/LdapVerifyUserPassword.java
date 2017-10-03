9b97e7c200fa7fb0f9703dfe503ecba7e7435b04
tam hon wa <tam_hon_wa-09@yahoo.com.hk>
2017-10-03 15:05:28 +0800

    aa

---
+++ MyJava/src/com/ldap/LdapVerifyUserPassword.java
@@ -0,0 +1,325 @@
+package com.ldap;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.TreeMap;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.BasicAttribute;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.ModificationItem;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+
+public class LdapVerifyUserPassword {
+
+	public static final String JAVA_HOME = "C:\\Program Files\\Java\\jdk1.8.0_131";
+	public static final ResourceBundle res = ResourceBundle.getBundle(LdapSearch.class.getName());
+
+	public static final SimpleDateFormat SDF_1 = new SimpleDateFormat("yyyyMMdd_hhmmss");
+	private String ldapAdServer = "";
+	private String ldapSearchBase = "";
+
+	private String ldapUsername = "";
+	private String ldapPassword = "";
+	
+	private boolean useSsl;
+	//private LdapContext ctx;
+	/**
+	 * args example
+	 * 
+	 * sun zzzB140081@adtest.cuhk.edu.hk abcd123$
+	 * central DDB103773@cuhk.edu.hk 7hh5v8?#
+	 * 
+	 * */
+	public static void main(String[] args)
+	{
+		String ldapSettingSet = "central";
+		String searchKeyword = "";
+		String ldapUsername="DDB103773@cuhk.edu.hk", ldapPassword="7hh5v8?#";
+/*		
+		DDA313700	wbsem8.$
+		DDA315700	yznvg6?.
+		DDB103770	x98vr7$$
+		DDB103771	g92hg5-?
+		DDB103773	7hh5v8?#
+*/
+		//searchKeyword = "displayName=aa,-() _'./";
+		if(args.length>0)
+		{
+			if(args[0]!=null)
+			{
+				ldapSettingSet = args[0].trim();
+			}
+			if(args[1]!=null)
+			{
+				ldapUsername = args[1].trim();
+			}
+			if(args[2]!=null)
+			{
+				ldapPassword = args[2].trim();
+			}
+		}
+		
+		new LdapVerifyUserPassword( ldapSettingSet, ldapUsername, ldapPassword );
+	}
+	
+	public LdapVerifyUserPassword(String ldapSettingSet,  
+			String ldapUsername, String ldapPassword)
+	{
+		System.out.println("host="+res.getString(ldapSettingSet+".host"));
+		System.out.println("searchbase="+res.getString(ldapSettingSet+".searchbase"));
+		System.out.println("username="+res.getString(ldapSettingSet+".username"));
+		System.out.println("password="+res.getString(ldapSettingSet+".password"));
+		System.out.println("ssl="+res.getString(ldapSettingSet+".ssl"));
+		
+		ldapAdServer = res.getString(ldapSettingSet+".host");
+		ldapSearchBase = res.getString(ldapSettingSet+".searchbase");
+		//ldapUsername = res.getString(ldapSettingSet+".username");
+		//ldapPassword = res.getString(ldapSettingSet+".password");
+		useSsl = "yes".equals(res.getString(ldapSettingSet+".ssl"));
+		
+		Hashtable<String, Object> env = new Hashtable<String, Object>();
+		env.put(Context.SECURITY_AUTHENTICATION, "simple");
+		if(ldapUsername != null)
+		{
+			env.put(Context.SECURITY_PRINCIPAL, ldapUsername);
+		}
+		if(ldapPassword != null)
+		{
+			env.put(Context.SECURITY_CREDENTIALS, ldapPassword);
+		}
+
+		if(useSsl)
+		{
+			env.put(Context.SECURITY_PROTOCOL, "ssl");
+			env.put(Context.PROVIDER_URL, "ldaps://"+ldapAdServer);
+		}
+		else
+		{
+			env.put(Context.PROVIDER_URL, "ldap://"+ldapAdServer);
+		}
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+		
+		//ensures that objectSID attribute values
+		//will be returned as a byte[] instead of a String
+		env.put("java.naming.ldap.attributes.binary", "objectSID");
+		
+		// the following is helpful in debugging errors
+		//env.put("com.sun.jndi.ldap.trace.ber", System.err);
+		
+		run( env, ldapSettingSet );
+	}
+	
+	public void run(Hashtable<String, Object> env, String ldapSettingSet)
+	{
+
+		LdapContext ctx = null;
+		try
+		{
+			System.out.println("java home: "+System.getProperty("java.home"));
+			System.setProperty("javax.net.ssl.keyStore", JAVA_HOME+"\\jre\\lib\\security\\cacerts");
+			System.setProperty("javax.net.ssl.trustStore", JAVA_HOME+"\\jre\\lib\\security\\cacerts");
+			
+			SearchControls ctls = new SearchControls();
+			ctls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+			ctls.setReturningAttributes(
+				new String[] {
+					"ds-cfg-listen-port",
+					"objectclass"
+				} );
+			
+			ctx = new InitialLdapContext( env, null );
+			
+			System.out.println("connected");
+			
+
+			
+		    
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if(ctx!=null)
+				{
+					ctx.close();
+				}
+			}
+			catch(Exception e)
+			{
+			}
+		}
+		System.out.println("LdapExample exit");
+	}
+	
+	private void modifyAttr(LdapContext ctx, String nameInNamespace, List<String> attributeNames, List<String> attributeValues) throws NamingException
+	{
+		if(attributeNames==null || attributeNames.size()==0)
+		{
+			return;
+		}
+	
+		ModificationItem[] mods = new ModificationItem[attributeNames.size()];
+
+	    //Attribute mod0 = new BasicAttribute("visible", "yes");
+
+		int i = 0;
+	    for(String attrName: attributeNames)
+	    {
+	    	mods[i] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, new BasicAttribute(attrName, attributeValues.get(i) ));
+	    	i++;
+	    }
+
+	    //ctx.modifyAttributes("uid=304f8d81-5bce11e7-80babd7d-ae3f753a,dc=cuhk,dc=edu,dc=hk", mods);
+	    ctx.modifyAttributes(nameInNamespace, mods);
+	}
+	
+	public NamingEnumeration<SearchResult> findAccount(DirContext ctx, 
+			String ldapSearchBase, String searchFilter) throws NamingException {
+
+		SearchControls searchControls = new SearchControls();
+		searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+
+		NamingEnumeration<SearchResult> results = ctx.search(ldapSearchBase, searchFilter, searchControls);
+
+		return results;
+	}
+	
+	public static void listResult(NamingEnumeration<SearchResult> results) throws NamingException
+	{
+		int i = 1;
+		SearchResult searchResult = null;
+		while(results.hasMoreElements())
+		{
+			searchResult = (SearchResult) results.nextElement();
+			System.out.println("============ record "+i+" ============");
+			System.out.println( searchResult.getNameInNamespace() );
+			Attributes attrs = searchResult.getAttributes();
+			NamingEnumeration enumer = attrs.getAll();
+			Attribute attrObj;
+			while( enumer.hasMore() )
+			{
+				attrObj = (Attribute)enumer.next();
+				//System.out.println( "attr: "+attrObj.getClass().getName() );
+				//System.out.println( "attr: "+attrObj );
+				
+				System.out.print( "name: "+attrObj.getID()+", " );
+				System.out.println( "value: "+attrObj.get() );
+				//System.out.println( "attr: "+(attrObj instanceof javax.naming.directory.Attribute) );
+				
+				//break;
+			}
+			i++;
+		}
+	}
+	
+	public static void listResultCsv(NamingEnumeration<SearchResult> results, String ldapSettingSet) throws NamingException, IOException
+	{
+		Calendar cal = Calendar.getInstance();
+		
+		BufferedWriter bw = new BufferedWriter(
+				new OutputStreamWriter(new FileOutputStream("D:\\Documents\\temp\\"+SDF_1.format(cal.getTime())
+						+"_"+ldapSettingSet+"_search.csv"), "utf8"));
+		//String csvCaption = "nameInNameSpace";
+		int i = 1;
+		SearchResult searchResult = null;
+		//StringBuffer sbValue;
+		
+		int captionSeq = 0;
+		
+		
+		// calculate caption
+		Map<String, Integer> captionMap = new TreeMap<String, Integer>();
+		List<Map<String, Object>> captionValueList = new ArrayList<Map<String, Object>>();
+		Map<String, Object> captionValue;
+		while(results.hasMoreElements())
+		{
+			searchResult = (SearchResult) results.nextElement();
+			System.out.println("============ record "+i+" ============");
+			System.out.println( searchResult.getNameInNamespace() );
+			Attributes attrs = searchResult.getAttributes();
+			NamingEnumeration enumer = attrs.getAll();
+			Attribute attrObj;
+			//sbValue = new StringBuffer( "\""+searchResult.getNameInNamespace()+"\"" );
+			captionValue = new TreeMap<String, Object>();
+			captionValue.put("nameInNameSpace", searchResult.getNameInNamespace() );
+			while( enumer.hasMore() )
+			{
+				attrObj = (Attribute)enumer.next();
+				
+				if(!captionMap.containsKey(attrObj.getID()))
+				{
+					captionMap.put(attrObj.getID(), captionSeq++);
+				}
+				
+				captionValue.put(attrObj.getID(), attrObj.get());
+				System.out.print( ""+attrObj.getID()+" = " );
+				System.out.println( ""+attrObj.get() );
+				
+			}
+			captionValueList.add(captionValue);
+			i++;
+		}
+		
+		// write caption
+		{
+			Set<Entry<String, Integer>> entrySet = captionMap.entrySet();
+			Iterator<Entry<String, Integer>> iter = entrySet.iterator();
+			bw.write( "nameInNameSpace" );
+			while(iter.hasNext())
+			{
+				bw.write( ","+iter.next().getKey() );
+			}
+			bw.newLine();
+		}
+		
+		// write data
+		for(Map<String, Object> captionValue2: captionValueList)
+		{
+			bw.write("\""+captionValue2.get("nameInNameSpace").toString()+"\"");
+			
+			Set<Entry<String, Integer>> entrySet = captionMap.entrySet();
+			Iterator<Entry<String, Integer>> iter = entrySet.iterator();
+			Object o;
+			while(iter.hasNext())
+			{
+				o = captionValue2.get( iter.next().getKey() );
+				if(o==null)
+				{
+					bw.write( ",");
+				}
+				else
+				{
+					bw.write( ",\""+o.toString()+"\"");
+				}
+			}
+			bw.newLine();
+			
+		}
+
+		bw.close();
+	}
+}
